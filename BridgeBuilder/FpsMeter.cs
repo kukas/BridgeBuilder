d0d303769fc068648917c9d7c830b22e04cef1ce
Jirka Balhar <balhar.j@gmail.com>
2017-07-09 20:24:15 +0200

    more stable fps meter

--- BridgeBuilder/FpsMeter.cs
+++ BridgeBuilder/FpsMeter.cs
@@ -6,23 +6,29 @@ namespace BridgeBuilder
     internal class FpsMeter
     {
         Stopwatch sw = new Stopwatch();
+        double[] history;
+        int len = 50;
+        int p = 0;
         double last = 0;
 
         public double FPS { get; internal set; }
 
         public FpsMeter()
         {
+            history = new double[len];
             sw.Start();
         }
 
         public int Next()
         {
             double now = sw.Elapsed.TotalMilliseconds;
-            double dt = now - last;
+            history[p] = now;
+            p = (p+1)%len;
+            double dt = (now - history[p])/len;
             last = now;
             FPS = 1000 / dt;
 
-            long goal = 1000 / 30;
+            long goal = 1000 / 20;
             int wait = Math.Max((int)(goal - dt), 0);
             return wait;
         }
